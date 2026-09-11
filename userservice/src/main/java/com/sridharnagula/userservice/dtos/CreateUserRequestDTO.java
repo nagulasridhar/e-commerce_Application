66c46cb6e35cb58bf38a8b98a23708c69d7fa861
nagulasridhar <sridharnagula200@gmail.com>
2026-09-11 07:35:54 +0530

    Controller added in user service

---
+++ userservice/src/main/java/com/sridharnagula/userservice/dtos/CreateUserRequestDTO.java
@@ -0,0 +1,23 @@
+package com.sridharnagula.userservice.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateUserRequestDTO {
+
+    @NotBlank(message = "name is required")
+    private String name;
+
+    @NotBlank(message = "email is required")
+    @Email(message = "email must be valid")
+    private String email;
+
+    private String phoneNumber;
+
+    @NotBlank(message = "password is required")
+    private String password;
+}
