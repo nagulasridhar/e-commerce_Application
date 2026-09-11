be3ebac0c8cff0dd6df20185ad6911c951f13b53
nagulasridhar <sridharnagula200@gmail.com>
2026-09-11 07:14:37 +0530

    clients and exceptions added in order service

---
+++ orderservice/src/main/java/com/sridharnagula/orderservice/exceptions/RemoteProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sridharnagula.orderservice.exceptions;
+
+public class RemoteProductNotFoundException extends Exception {
+    public RemoteProductNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ orderservice/src/main/java/com/sridharnagula/orderservice/exceptions/RemoteUserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sridharnagula.orderservice.exceptions;
+
+public class RemoteUserNotFoundException extends Exception {
+    public RemoteUserNotFoundException(String message) {
+        super(message);
+    }
+}
